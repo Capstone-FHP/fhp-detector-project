776a88330eba1f3e2ebd82bcd5e5c05b74368ce4
4783926y <fdsq7586@gmail.com>
2026-01-05 15:20:05 +0900

    매인액티비티에 불필요한 부분 제거

--- mobile-front-end/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ mobile-front-end/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,23 +16,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 1. 툴바를 액션바로 설정하거나, 그냥 네비게이션 아이콘 클릭 리스너 달기
-        binding.toolbar.setNavigationOnClickListener {
-            // 왼쪽 끝(START)에서 메뉴 열기
-            binding.drawerLayout.openDrawer(GravityCompat.START)
-        }
-
-        // 2. 슬라이드 메뉴(NavigationView) 클릭 리스너
-        binding.navView.setNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> Toast.makeText(this, "홈 클릭", Toast.LENGTH_SHORT).show()
-                R.id.nav_search -> Toast.makeText(this, "검색 클릭", Toast.LENGTH_SHORT).show()
-                R.id.nav_profile -> Toast.makeText(this, "프로필 클릭", Toast.LENGTH_SHORT).show()
-            }
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-            true
-        }
-
         // 1번 버튼: 주요 기능 화면으로 이동
         binding.menuBtn1.setOnClickListener {
             val intent = Intent(this, ActionActivity::class.java)
@@ -45,14 +26,6 @@ class MainActivity : AppCompatActivity() {
         binding.menuBtn2.setOnClickListener {
             val intent = Intent(this, StatisticActivity::class.java)
             startActivity(intent)
-        }
-    }
-
-    override fun onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
         }
     }
 }
