7aa15301ce0ac7c2a2b1f9adf0805d4e7dee7fa7
4783926y <fdsq7586#gmail.com>
2026-03-07 16:01:25 +0900

    웹 구현 카메라로 좌표 인식하고 영점 잡는거 까지

---
+++ src/services/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// TODO: 팀원이나 본인의 Firebase 콘솔에서 발급받은 실제 설정값으로 바꿔야 합니다!
+const firebaseConfig = {
+    apiKey: "AIzaSyBRco2eF8R9PXdj-ZcWkmpI5XGtUZFMGSM",
+    authDomain: "fhp-detector.firebaseapp.com",
+    projectId: "fhp-detector",
+    storageBucket: "fhp-detector.firebasestorage.app",
+    messagingSenderId: "707547024162",
+    appId: "1:707547024162:web:f85c8d3af6d04063c5f37b"
+};
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+// 인증(로그인) 기능 내보내기
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
