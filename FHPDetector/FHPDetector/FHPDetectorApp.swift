27dbfdb63bfb645939fbd6dc91919de8b57eaa0c
chanwoo <chanwoo@chanwooui-MacBookAir.local>
2026-05-07 01:05:08 +0900

    iOS 프로젝트 초기 세팅

---
+++ FHPDetector/FHPDetector/FHPDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FHPDetectorApp.swift
+//  FHPDetector
+//
+//  Created by chanwoo on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FHPDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
